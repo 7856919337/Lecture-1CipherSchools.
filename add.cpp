420998901d73367010e0c84b5df60c34865d8846
7856919337 <114799990+7856919337@users.noreply.github.com>
2024-06-13 15:09:00 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//
+//int a,b,c,d;
+//cout<<"Enter your first no=";
+//cin>>a;
+//cout<<"Enter your second no=";
+//cin>>b;
+//cout<<"Enter your choice, for add =1,for substract =2,for multiplicatio =3:";
+//cin>>c;
+//if(c==1){
+//	d=a+b;
+//	cout<<d;
+//}
+//else if(c==2){
+//	d=a-b;
+//	cout<<d;
+//}
+//else{
+//	d=a*b;
+//	cout<<d;
+//}}
+int main(){
+	int m;
+	m=2/3(3*4);
+	cout<<m;
+	
+}
